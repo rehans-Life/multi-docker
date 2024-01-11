fa19927fd496c35106662bec7d395d93b6a1779e
rehan <rehantosif4@gmail.com>
2024-01-11 14:48:34 +0300

    travis ci deployment with multi containers setup

---
+++ server/index.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const cors = require('cors');
+const { createClient } = require("redis");
+const { Pool } = require('pg');
+
+const keys = require("./keys");
+const env = process.env.NODE_ENV;
+
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+app.use(cors());
+
+const pgClient = new Pool({
+    user: keys.pgUser,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    port: keys.pgPort,
+    password: keys.pgPasword,
+    ssl: env !== 'production' ? false : { rejectUnauthorized: true }
+});
+
+pgClient.on('error', () => console.log('Lost PG connection'));
+
+const redisClient = createClient({
+    socket: {
+        host: keys.redisHost,
+        port: keys.redisPort,
+        reconnectStrategy: () => 1000
+    },
+});
+
+const redisPublisher = redisClient.duplicate();
+
+app.get('/', (_, res) => {
+    res.status(200).send('Bye there');
+});
+
+app.get('/values/all', async (req, res) => {
+    const values = await pgClient.query('SELECT * from values');
+    res.status(200).send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+    const values = await redisClient.hGetAll('values');
+    res.status(200).send(values);
+});
+
+app.post('/values', async (req, res) => {
+    const index = req.body.index;
+
+    if(index > 40) {
+        res.send(422).send({error: "Index too high"});
+        return;
+    }  
+
+    await redisClient.hSet('values', index, 'Nothing yet');
+    await redisPublisher.publish('insert', index);
+
+    await pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+    res.status(200).send({
+        working: true,
+    })
+});
+
+(async () => {
+    try {
+        await pgClient.connect();
+        await pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)');
+
+        await redisClient.connect();
+        await redisPublisher.connect();
+    
+        app.listen(port , () => console.log(`Server listening on port ${port}`));    
+    } catch (err) {
+        console.log(err);
+
+        await pgClient.end();
+        await redisClient.disconnect();
+        await redisPublisher.disconnect();
+
+        process.exit(1);
+    }
+})()
+
---
+++ worker/index.js
@@ -0,0 +1,43 @@
+const redis = require("redis");
+const keys = require("./keys");
+
+const redisClient = redis.createClient({
+    socket:{
+        host: keys.redistHost,
+        port: keys.redisPort,
+        reconnectStrategy: () => 1000
+    },
+});
+const sub = redisClient.duplicate();
+
+function fibonacci(index) {
+    const cache = {};
+
+    function helper(index) {
+        if(index < 2) return index;
+        cache[index] = helper(index - 1) + helper(index - 2);
+        return cache[index];
+    }
+
+    return helper(index);
+}
+
+(async () => {
+    try {
+        await redisClient.connect();
+        
+        sub.subscribe('insert', async (message) => {
+            await redisClient.hSet('values', message, fibonacci(parseInt(message)));
+        });
+
+        await sub.connect();    
+    } catch (err) {
+        console.log(err);
+
+        await sub.disconnect();
+        await redisClient.disconnect();
+
+        process.exit(1);
+    }
+})();
+
