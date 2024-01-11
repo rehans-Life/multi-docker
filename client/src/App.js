a2c1993a638079a0a0dd194f8adb7c7ede31c703
rehan <rehantosif4@gmail.com>
2024-01-11 14:58:20 +0300

    added travis.ci setup

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import Otherpage from './Otherpage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo'/>
+          <h1 className='App-title'>Welcome to React</h1>
+          <Link to='/'>Home</Link>
+          <Link to='/other'>Other Page</Link>
+        </header>
+        <Routes>
+          <Route exact path='/other' element={<Otherpage/>}/>
+          <Route exact path='/' element={<Fib />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
---
+++ client/src/Fib.js
@@ -0,0 +1,74 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+export default function Fib() {
+    const [values, setValues] = useState({});
+    const [index, setIndex] = useState('');
+    const [seenIndexes, setSeenIndexes] = useState([]);
+
+    async function fetchValues(){        
+        try {
+            const { data } = await axios.get('/api/values/current');
+            setValues(data);    
+        } catch (err) {console.log(err)}
+    }
+
+    async function fetchIndexes() {
+        try {
+            const { data } = await axios.get('/api/values/all');
+            setSeenIndexes(data);    
+        } catch (err) {console.log(err)}
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        try {
+            await axios.post('/api/values', {
+                index
+            });
+    
+        } catch (err) {
+            console.log(err);
+        }
+
+        setIndex('');    
+    }
+
+    useEffect(() => {
+        fetchValues();
+        fetchIndexes();
+    }, []);
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Enter your index:
+                </label>
+                <input value={index} onChange={(e) => setIndex(e.target.value)}/>
+                <button>Submit</button>
+            </form>
+
+            <h3>Indexes I have seen:</h3>
+            <div>
+                {
+                    seenIndexes.map(({ number }, index) => 
+                        <span key={index}>{number},</span>
+                    )
+                }
+            </div>
+
+            <h3>Calculated Values</h3>
+            <div>
+                {
+                    Object.entries(values).map((value, index) => 
+                        <div key={index}>
+                            {`For index${value[0]} I calculated ${value[1]}`}
+                        </div>
+                    )
+                }
+            </div>
+        </div>
+    )
+}
